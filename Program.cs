60b22deb2574e71402328c896588aae3d4e17818
LARA MICAELA FARINA <47871657@ort>
2023-04-10 10:12:27 -0300

    Funcion OBtenerBoxeador

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@
     static int Menu()
     {
         int opcion;
+        string nom = "", pais = "";
+        int peso = 0, pg = 0, vp = 0;
         Console.WriteLine("1. Cargar datos Boxeador 1");
         Console.WriteLine("2. Cargar datos Boxeador 2");
         Console.WriteLine("3. Pelear!");
@@ -15,9 +17,14 @@
         switch (opcion)
         {
             case 1:
+                Boxeador boxeador1 = new Boxeador(nom, pais, peso, pg, vp);
+
+                boxeador1 = ObtenerBoxeador();
                 break;
             case 2:
-                break;
+                Boxeador boxeador2 = new Boxeador(nom, pais, peso, pg, vp);
+
+                boxeador2 = ObtenerBoxeador(); break;
             case 3:
                 break;
             case 4:
@@ -25,7 +32,7 @@
         }
         return opcion;
     }
-    static void CargarBoxeador1()
+    static Boxeador ObtenerBoxeador()
     {
         string nom, pais;
         int peso, pg, vp;
@@ -42,7 +49,15 @@
             vp = IngresarEntero("Ingrese la velocidad de piernas del boxeador");
 
         } while (vp < 0 && vp > 100);
-        Boxeador boxeador1 = new Boxeador(nom, pais, peso, pg, vp);
+        Boxeador b = new Boxeador(nom, pais, peso, pg, vp);
+        Console.WriteLine($"Se ha creado el boxeador {nom}");
+        return b;
+    }
+
+
+    static void Pelear()
+    {
+
     }
     static int IngresarEntero(string msg)
     {
