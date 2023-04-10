cbbf3100bfb9cfd4bdace4f0b241a53ad27de084
LARA MICAELA FARINA <47871657@ort>
2023-04-10 10:27:54 -0300

    Programa Terminado

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@
         int opcion;
         string nom = "", pais = "";
         int peso = 0, pg = 0, vp = 0;
+        Boxeador boxeador1 = new Boxeador(nom, pais, peso, pg, vp);
+        Boxeador boxeador2 = new Boxeador(nom, pais, peso, pg, vp);
+
+
         Console.WriteLine("1. Cargar datos Boxeador 1");
         Console.WriteLine("2. Cargar datos Boxeador 2");
         Console.WriteLine("3. Pelear!");
@@ -17,15 +21,14 @@
         switch (opcion)
         {
             case 1:
-                Boxeador boxeador1 = new Boxeador(nom, pais, peso, pg, vp);
 
                 boxeador1 = ObtenerBoxeador();
                 break;
             case 2:
-                Boxeador boxeador2 = new Boxeador(nom, pais, peso, pg, vp);
 
                 boxeador2 = ObtenerBoxeador(); break;
             case 3:
+            Pelear(boxeador1,boxeador2);
                 break;
             case 4:
                 break;
@@ -55,8 +58,52 @@
     }
 
 
-    static void Pelear()
+    static void Pelear(Boxeador boxeador1, Boxeador boxeador2)
     {
+        if (boxeador1 != null && boxeador2 != null)
+        {
+            double skill1;
+            double skill2;
+            skill1 = boxeador1.ObtenerSkill();
+            skill2 = boxeador2.ObtenerSkill();
+            if (skill1 > skill2)
+            {
+                if ((skill1 - skill2) >= 30)
+                {
+                    Console.WriteLine($"Ganó {boxeador1.Nombre} por KO");
+                }
+                else if ((skill1 - skill2) >= 10 && (skill1 - skill2) < 30)
+                {
+                    Console.WriteLine($"Ganó {boxeador1.Nombre} por puntos en fallo unánime");
+                }
+                else
+                {
+                    Console.WriteLine($"Ganó {boxeador1.Nombre} por puntos en fallo dividido");
+                }
+            }
+            else if (skill2 > skill1)
+            {
+                if ((skill2 - skill1) >= 30)
+                {
+                    Console.WriteLine($"Ganó {boxeador2.Nombre} por KO");
+                }
+                else if ((skill2 - skill1) >= 10 && (skill2 - skill1) < 30)
+                {
+                    Console.WriteLine($"Ganó {boxeador2.Nombre} por puntos en fallo unánime");
+                }
+                else
+                {
+                    Console.WriteLine($"Ganó {boxeador2.Nombre} por puntos en fallo dividido");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Los dos boxeadores empataron");
+            }
+        } else{
+            Console.WriteLine("Uno de los boxeadores (O ninguno) no existe.");
+        }
+
 
     }
     static int IngresarEntero(string msg)
